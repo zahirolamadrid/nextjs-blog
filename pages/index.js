f4ea226a08f4767f0a404341eae7619f7d4ece5e
Zahiro Lamadrid <zahirolamadridalmeida@gmail.com>
2023-08-23 11:55:21 -0600

    Tutorial de React TicTacToe

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home({ allPostsData }) {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <p><Link href={`/tic-tac-toe`}>Juego de Tic-Tac-Toe</Link></p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
